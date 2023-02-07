9e4d68047ee2e20d14996ce0954897683b6f8957
Caleb Markovich <calebmarkovich@gmail.com>
2023-02-07 12:18:52 -0600

    first commit

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,68 @@
+const request = require('supertest');
+const app = require('../../app');
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+
+describe('Launches Api', () => {
+  beforeAll(async () => {
+    await mongoConnect();
+  });
+
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
+
+  describe('Test GET /v1/launches', () => {
+    test('It should respond with 200 success', async () => {
+      const response = await request(app)
+        .get('/v1/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+  });
+
+  describe('Test POST /v1/launches', () => {
+    const completeLaunchData = {
+      mission: 'USS Enterprise',
+      rocket: 'NC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'February 1, 2022',
+    };
+
+    test('It should respond with 201 created', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+      expect(response.body).toMatchObject({
+        ...completeLaunchData,
+        launchDate: new Date(completeLaunchData.launchDate).toISOString(),
+      });
+    });
+
+    test('It should catch missing required properties', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send({ ...completeLaunchData, mission: null })
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: 'Missing required launch property',
+      });
+    });
+
+    test('It should catch invalid dates', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send({ ...completeLaunchData, launchDate: 'hello' })
+        .expect('Content-Type', /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: 'Invalid launch date',
+      });
+    });
+  });
+});
